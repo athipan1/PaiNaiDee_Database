532e7a05eb43445bee185f7aa57ed55fcb6925d6
athipan1 <166430089+athipan1@users.noreply.github.com>
2025-07-25 01:31:46 +0700

    Create recommender.py

---
+++ api/recommender.py
@@ -0,0 +1,5 @@
+def recommend_for_user(db, user_id):
+    from .models import Attraction, Review
+    subq = db.query(Review.attraction_id).filter(Review.user_id == user_id)
+    recs = db.query(Attraction).filter(~Attraction.id.in_(subq)).limit(5).all()
+    return recs
