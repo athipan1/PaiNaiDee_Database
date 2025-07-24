1652994b8fe4ea5f80e91f87cb3a1a8941fd398a
athipan1 <166430089+athipan1@users.noreply.github.com>
2025-07-25 01:28:57 +0700

    Create schemas.py

---
+++ api/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class AttractionOut(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    address: Optional[str] = None
+    province: Optional[str] = None
+    district: Optional[str] = None
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+    opening_hours: Optional[str] = None
+    entrance_fee: Optional[str] = None
+    website: Optional[str] = None
+    main_image_url: Optional[str] = None
+
+    class Config:
+        orm_mode = True
